a8002ee5653d1d3d10351267c09b47428f20af6f
jihye <mkos__@nate.com>
2023-04-14 18:25:33 +0900

    static설정 다시하기 아직연결안됨

--- django_pjt/settings.py
+++ django_pjt/settings.py
@@ -120,7 +120,12 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
+# STATIC_URL = '/static/'
+
 STATIC_URL = '/static/'
+
+STATIXFILES_DIRS = (BASE_DIR / 'static',)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
