d156b2d44fa8b9b12367ab4bfb8f50c5c9a6db72
jihye <mkos__@nate.com>
2023-04-16 03:51:13 +0900

    main 수정

--- django_pjt/settings.py
+++ django_pjt/settings.py
@@ -124,8 +124,9 @@ USE_TZ = False
 # STATIC_URL = '/static/'
 
 STATIC_URL = '/static/'
-
-STATIXFILES_DIRS = (BASE_DIR / 'static',)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
