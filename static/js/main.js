4dbd973040de661ae59a9e0aba6238e48b0b7b98
jihye <mkos__@nate.com>
2023-04-25 04:36:10 +0900

    main 수정

---
+++ static/js/main.js
@@ -0,0 +1,7 @@
+
+
+// setTimeout(function(){
+//   $(".slab .slab-wrapper").css("display", "none");
+//   $(".poster-text").addClass("show");
+//   $(".slab-wrapper").addClass("show");
+// }, 6500);
