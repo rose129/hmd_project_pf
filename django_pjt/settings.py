4efd907f55d7e4a9190e2da376d8a825f76eae9c
jihye <mkos__@nate.com>
2023-04-25 18:27:38 +0900

    포트폴리오 마무리

--- django_pjt/settings.py
+++ django_pjt/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
+from . import myenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-abizt5)49l5*u*o8tw)r4#=^fky!niyn7c6j%ljxmb2^%a2k91'
+SECRET_KEY = myenv.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = myenv.DEBUG
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1']
+ALLOWED_HOSTS = myenv.ALLOWED_HOSTS
 
 
 # Application definition
@@ -98,12 +100,7 @@ WSGI_APPLICATION = 'django_pjt.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = myenv.DATABASES
 
 
 # Password validation
